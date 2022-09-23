73028c5a0a76b2437405163db29a61953efc16a5
HEEWON-SHIN <113659480+HEEWON-SHIN@users.noreply.github.com>
2022-09-23 11:18:11 +0900

    충돌 해결..?

---
+++ avito/src/singleProduct/reviewBean.java
@@ -0,0 +1,15 @@
+package singleProduct;
+
+public class reviewBean {
+
+	
+	private int rNo;
+	private String email;
+	private String name;
+	private String rContent;
+	private int rPtNo;
+	
+	
+	
+	
+}
