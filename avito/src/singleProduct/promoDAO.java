dcb51154b906b0da3244154622aa4059a9f9e124
shw <ise03054@gmailcom>
2022-10-03 15:10:52 +0900

    거의 다 왔다....

---
+++ avito/src/singleProduct/promoDAO.java
@@ -0,0 +1,105 @@
+package singleProduct;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class promoDAO {
+	
+	
+	Connection con;
+
+	PreparedStatement pstmt;
+
+	ResultSet rs;
+
+	String sql;
+	
+
+	/* DB연결 메소드 */
+	private  Connection getCon() throws Exception{
+		
+		// DB와 연결을 맺은 접속정보를 지닌 Connection객체를 저장할 변수
+		Connection con = null;
+		
+		
+			// 1.WAS서버와 연결된 DBApp웹프로젝트의 모든 정보를 가지고 있는 컨텍스트객체 생성
+			Context init = new InitialContext();
+			// 2.연결된 WAS서버에서 DataSource(커넥션풀) 검색해서 가져오기
+			DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/jspbeginner");
+			// DataSource(커넥션풀)에서 DB연동객체 (커넥션) 가져오기
+			con = ds.getConnection(); // DB연결
+			
+			return con;
+
+	}// getCon()메소드 끝
+
+	private void close() {
+
+		try {
+			if (rs != null)// rs가 사용 중이라면..?
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();// 커넥션 풀로 Connection객체 사용 후 반납
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}// close메소드 끝
+
+	
+	/*DB에 프로모션코드 발급 이력있는 회원 조회*/
+	public boolean usedCheck(String email) {
+
+		boolean check =false;
+		
+		try {
+			con = getCon();
+			sql = "select * from promocode where email=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, email);
+			rs = pstmt.executeQuery();
+			check = rs.next();//이미 발급 이력이 있으면 true
+				
+		} catch (Exception e) {
+			System.out.println("usedCheck메소드 내부 오류 발생 : " +e);
+			e.printStackTrace();
+			check = false;//발급 이력 없으면(조회 안되면) false
+		}finally {
+			close();
+		}
+		
+		
+		return check;
+	}
+
+	
+	/*프로모션 코드 전송에 성공하면 propocode테이블에 코드 발급받은 이메일 저장*/
+	public void addEmail(String email) {
+		
+		try {
+			con = getCon();
+			sql = "insert into promocode (email) set (?) ";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, email);
+			pstmt.executeUpdate();			
+			
+		} catch (Exception e) {
+			System.out.println("addEmail메소드 오류 발생:"+e );
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+	}
+	
+	
+	
+}
